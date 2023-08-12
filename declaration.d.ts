f3cda7171d0df1d8cf2649966908f9f03ac9a96e
Andy Willis <awillis@fastmail.fm>
2023-08-12 13:30:01 +0100

    Added animal images, updated dataset

--- declaration.d.ts
+++ declaration.d.ts
@@ -1,9 +1,2 @@
 declare module '*.module.css';
 declare module '*.ico';
-
-declare namespace React {
-  interface HTMLAttributes<T> {
-    // Preact supports using "class" instead of "classname" - need to teach typescript
-    class?: string;
-  }
-}
