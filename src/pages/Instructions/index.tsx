56d3b98646f82f8c6c7d2cf8a995230033306a2e
Andy Willis <awillis@fastmail.fm>
2023-08-16 16:09:13 +0100

    Fixed sizing

--- src/pages/Instructions/index.tsx
+++ src/pages/Instructions/index.tsx
@@ -23,9 +23,9 @@ function Instructions() {
 				</header>
 				<ul>
 					<li>Each card has a set of statistics. Your aim in each round is to choose a statistic higher than the corresponding one on the other player&apos;s card.</li>
-					<li>If it&apos;s higher than the opponent&apos;s card the player wins the round and the player&apos;s card.</li>
-					<li>If it&apos;s lower the other player wins the round and the player&apos;s card.</li>
-					<li>If it&apos;s a tie the cards are placed on the board and a new round begins. The winner of the next round gets all the cards.</li>
+					<li>If it&apos;s higher than the opponent&apos;s card the player wins the round and the opponent&apos;s card.</li>
+					<li>If it&apos;s lower the opponent wins the round and the player&apos;s card.</li>
+					<li>If it&apos;s a tie the cards are placed on the board and a new round begins. The winner of the next round gets the winning card and all the cards on the board.</li>
 				</ul>
 			</section>
 		</main>
