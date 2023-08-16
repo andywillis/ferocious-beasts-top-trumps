49c0dc2bea09454669933c60c4f35a948db2731f
Andy Willis <awillis@fastmail.fm>
2023-08-16 17:38:04 +0100

    Added shadow, fixed padding

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { default as Board } from './Board';
 export { default as Card } from './Card';
-export { default as Footer } from './Footer';
 export { default as Grid } from './Grid';
 export { default as Header } from './Header';
 export { default as MessageBox } from './MessageBox';
